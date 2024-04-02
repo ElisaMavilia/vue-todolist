9d1dd5c6be57b5b53e03a96b0aef95758bf79b04
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-02 15:00:26 +0200

    Added createApp

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 import {todo} from './data.js';
+
+const {createApp} = Vue;
+
+createApp ({
+    data(){
+        return {
+
+        }
+    },
+     methods:{
+
+    },
+    mounted:{
+        
+    }
+}).mount('#app');
+
